097ca7828f6291208d33ea361dc0d8da4327b004
Parada <paradafaii28@gmail.com>
2020-08-04 20:31:30 -0600

    add geocoding api

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const request = require('request')
 require('dotenv').config()
-const api_key = process.env.API_KEY
+const weather_api_key = process.env.API_KEY
+const geocod_api_key = process.env.GEOCODING_API_KEY
 
 
 
-const url = `http://api.weatherstack.com/current?access_key=${api_key}&query=37.8267,-122.4233&units=f`
+const weatherURL = `http://api.weatherstack.com/current?access_key=${weather_api_key}&query=37.8267,-122.4233&units=f`
 
-request({ url: url, json: true }, (error, response) => {
+request({ url: weatherURL, json: true }, (error, response) => {
     // console.log(response.body.current)
-    console.log('It is currently ' + response.body.current.temperature + ' degree out. ' + 'It feels like ' + response.body.current.feelslike + ' degreess out.')
+    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degree out. ' + 'It feels like ' + response.body.current.feelslike + ' degreess out.')
+})
+
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${geocod_api_key}&limit=1`
+
+request({ url: geocodeURL, json: true}, (error, response ) => {
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+    console.log(latitude, longitude)
 })
