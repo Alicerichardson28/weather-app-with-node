5675fc8075620a6df06e4c9760f9fbe3745c2bc6
Parada <paradafaii28@gmail.com>
2020-08-04 21:00:03 -0600

    add handling error

--- app.js
+++ app.js
@@ -17,10 +17,16 @@ request({ url: weatherURL, json: true }, (error, response) => {
     }
 })
 
-const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${geocod_api_key}&limit=1`
+const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/colorado.json?access_token=${geocod_api_key}&limit=1`
 
 request({ url: geocodeURL, json: true}, (error, response ) => {
-    const latitude = response.body.features[0].center[1]
-    const longitude = response.body.features[0].center[0]
-    console.log(latitude, longitude)
+    if (error) {
+        console.log('Unable to connect to location services')
+    } else if (response.body.features.length === 0) {
+        console.log('Unable to find this location, please try a different location')
+    } else {
+        const latitude = response.body.features[0].center[1]
+        const longitude = response.body.features[0].center[0]
+        console.log(latitude, longitude)
+    }
 })
