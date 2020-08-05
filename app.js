bcc32add6f525d8e9844041cc5a089c78dfb76a2
Parada <paradafaii28@gmail.com>
2020-08-04 20:48:04 -0600

    add handling error

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const geocod_api_key = process.env.GEOCODING_API_KEY
 const weatherURL = `http://api.weatherstack.com/current?access_key=${weather_api_key}&query=37.8267,-122.4233&units=f`
 
 request({ url: weatherURL, json: true }, (error, response) => {
-    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degree out. ' + 'It feels like ' + response.body.current.feelslike + ' degreess out.')
+    if (error) {
+        console.log('Unable to connect to weather service')
+    } else if (response.body.error) {
+        console.log('Unable to find location')
+    } else {
+        console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degree out. ' + 'It feels like ' + response.body.current.feelslike + ' degreess out.')
+    }
 })
 
 const geocodeURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${geocod_api_key}&limit=1`
